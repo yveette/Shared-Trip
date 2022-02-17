290f3017e683f0d80f47d2e826f3c95a68e58d88
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-17 21:28:28 +0200

    Add user functionality to post a trip in database

--- controllers/trip.js
+++ controllers/trip.js
@@ -9,9 +9,28 @@ router.get('/create', isUser(), (req, res) => {
     res.render('create', { title: 'Create Trip Offer', data: {} });
 });
 
-router.post('/create', (req, res) => {
-    console.log(req.body);
-    res.redirect('/trips');
+router.post('/create', async (req, res) => {
+    const trip = {
+        start: req.body.start,
+        end: req.body.end,
+        date: req.body.date,
+        time: req.body.time,
+        carImg: req.body.carImg,
+        carBrand: req.body.carBrand,
+        seats: req.body.seats,
+        price: req.body.price,
+        description: req.body.description,
+        owner: req.session.user._id
+    };
+
+    try {
+        await createTrip(trip);
+        res.redirect('/trips');
+    } catch (err) {
+        console.error(err);
+        const errors = mapErrors(err);
+        res.render('create', { title: 'Create Trip Offer', data: trip, errors });
+    }
 });
 
 module.exports = router;
