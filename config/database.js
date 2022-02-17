4af136389885f43991feca319ca649f79e7ac2ba
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-17 12:21:51 +0200

    Added configuration to the project

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('../models/User');
+
+// TODO change database name
+const dbName = 'sharedTrips';
+const connnectionString = `mongodb://localhost:27017/${dbName}`;
+
+module.exports = async (app) => {
+    try {
+        mongoose.connect(connnectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Database connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('Database error');
+            console.error(err);
+        });
+    } catch (err) {
+        console.error('Error connecting to database');
+        process.exit(1);
+    }
+
+}
