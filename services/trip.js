91603cf640a5982ed4b3e31dc5aa8b88d16b510b
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-18 17:43:40 +0200

    Update user trips array by creating trip

--- services/trip.js
+++ services/trip.js
@@ -1,4 +1,5 @@
 const Trip = require('../models/Trip');
+const User = require('../models/User');
 
 async function getAllTrips() {
     return Trip.find({}).populate('owner');
@@ -20,6 +21,11 @@ async function getTripAndUsers(id) {
 async function createTrip(trip) {
     const result = new Trip(trip);
     await result.save();
+
+    //add trips id to user trips array
+    const user = await User.findById(result.owner);
+    user.trips.push(result._id);
+    await user.save();
 }
 
 async function updateTrip(id, trip) {
